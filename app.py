e5cd458f27f8f5850179f78981f41593daada50a
Jaray-zhou <61246636+Jaray-zhou@users.noreply.github.com>
2020-05-14 21:03:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import random
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def hello_world():
 	# here we use 'render_template' to link to the html template
 	return render_template('home.html')
+
+# add the function of amend user
+@app.route('/users')
+def amend_users():
+	# link to the users.html
+	return redirect('/static/users.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
